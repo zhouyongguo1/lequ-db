eb72878c76c5fe8fa7a0c2ae6269f7861f18606f
Zhou Yongguo <zhouyongguo@wisdomgarden.com>
2016-01-21 23:18:23 +0800

    modify team table

--- mysql/V20160110_1000__create_team.sql
+++ mysql/V20160110_1000__create_team.sql
@@ -1,5 +1,7 @@
 CREATE TABLE core_team (
 	`id` int unsigned NOT NULL AUTO_INCREMENT,
 	`name` nvarchar(100) NOT NULL,
+	`created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`created_by` int unsigned DEFAULT NULL,
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
